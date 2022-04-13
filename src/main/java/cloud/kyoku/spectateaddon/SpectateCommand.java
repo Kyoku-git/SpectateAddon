98ce55bafe4731332aed753aa98cd0f904dbac26
Kyoku <93015740+Kyoku-git@users.noreply.github.com>
2022-04-13 07:44:21 -0400

    Update SpectateCommand.java

--- src/main/java/cloud/kyoku/spectateaddon/SpectateCommand.java
+++ src/main/java/cloud/kyoku/spectateaddon/SpectateCommand.java
@@ -8,15 +8,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class SpectateCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        // wait for MBedwars to load
         BedwarsAPI.onReady(() -> {
 
+            // start of command code
             if (sender instanceof Player) {
                 Player p = (Player) sender;
                 if (p.hasPermission("bw.specplayer")) {
@@ -28,14 +28,14 @@ public class SpectateCommand implements CommandExecutor {
                             Arena arena = GameAPI.get().getArenaByPlayer(target);
                             if (arena != null && target != p) {
                                 arena.addSpectator(p);
-                                p.sendMessage(ChatColor.GREEN + "You are now spectating " + target.getDisplayName() + ".");
+                                p.sendMessage(ChatColor.GREEN + "You are now spectating " + target.getDisplayName() + ChatColor.GREEN + ".");
                             } else if (target == p){
                                 p.sendMessage(ChatColor.RED + "You cannot spectate yourself.");
                             } else {
-                                p.sendMessage(ChatColor.RED + "" + target.getDisplayName() + " is not in a game.");
+                                p.sendMessage(ChatColor.RED + "" + target.getDisplayName() + ChatColor.RED + " is not in a game.");
                             }
-                        } else if (p.hasPlayedBefore()) {
-                        p.sendMessage(ChatColor.RED + args[0] + " is currently offline.");
+                        } else if (target.hasPlayedBefore()) {
+                            p.sendMessage(ChatColor.RED + target.getDisplayName() + ChatColor.RED + " is currently offline");
                         } else {
                             p.sendMessage(ChatColor.RED + "Could not find player " + args[0]);
                         }
@@ -43,7 +43,7 @@ public class SpectateCommand implements CommandExecutor {
                 } else {
                     p.sendMessage(ChatColor.RED + "You do not have permission to use that command!");
                 }
-                } else {
+            } else {
                 Bukkit.getLogger().warning("Only players can use that command.");
             }
 
